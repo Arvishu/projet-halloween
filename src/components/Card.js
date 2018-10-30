b19a16f9d09b00a0c6f68eb293f22142015df88b
lydia.dal@live.fr <lydia.dal@live.fr>
2018-10-30 22:25:14 +0100

    push du jeu

---
+++ src/components/Card.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import './FourthPage.css'
+
+export default class Card extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+        
+        }
+      }
+    clicked(framework){
+      this.props.click(framework)
+    }
+
+  render() {
+    return (
+        <div className={"card" + (!this.props.close ? ' opened' : '') + (this.props.complete ? ' matched' : '')} onClick={() => this.clicked(this.props.framework)}>
+        <div className="front">
+          
+        </div>
+        <div className="back">
+          <img alt="image" src={"https://raw.githubusercontent.com/Lydia-Dali/png-halloween/master/" + this.props.framework + ".png"}/>
+        </div>
+      </div>
+    )
+  }
+}
--- src/components/FourthPage.js
+++ src/components/FourthPage.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
+import Card from './Card.js';
 import './FourthPage.css';
+import Playground from './Playground.js';
 
 export default class FourthPage extends Component {
+
+  
+
   render() {
     return (
-      <div>
-        <h1>bienvenue sur la page 4</h1>
+      <div className="background-fourthPage">
+        <img className="ghost"
+     src="https://media.giphy.com/media/aKQQzhoLRIzok/giphy.gif"
+     alt="ghost"/>
+      <img className="ghost1"
+     src="https://media.giphy.com/media/aKQQzhoLRIzok/giphy.gif"
+     alt="ghost"/>
+      <img className="ghost2"
+     src="https://media.giphy.com/media/aKQQzhoLRIzok/giphy.gif"
+     alt="ghost"/>
+     <img className="flambeau"
+     src="https://img1.liveinternet.ru/images/attach/b/4/103/121/103121361_fxhh4zbyhya.gif"
+     alt="flambeau"/>
+      <div id="gapp">
+      <Playground />
       </div>
+      </div>  
     )
   }
 }
