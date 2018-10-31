5ac09db4c77a1c5c9f74452415ce78d91625a77d
Hugo Arvis <arvishugo@hotmail.fr>
2018-10-31 11:40:07 +0100

    push v2

--- src/components/FourthPage.js
+++ src/components/FourthPage.js
@@ -22,6 +22,9 @@ export default class FourthPage extends Component {
      <img className="flambeau"
      src="https://img1.liveinternet.ru/images/attach/b/4/103/121/103121361_fxhh4zbyhya.gif"
      alt="flambeau"/>
+     <img className="eyes"
+     src="https://media2.giphy.com/media/Hm9GrdOICMoKs/giphy.gif?cid=3640f6095bd96e656268514f674667aa"
+     alt="eyes"/>
       <div id="gapp">
       <Playground />
       </div>
